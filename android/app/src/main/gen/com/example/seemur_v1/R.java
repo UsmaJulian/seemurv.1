6ec35db79a0d7be8f1ca2184be6fee6dc9f347f6
tostaco <jeusmag86@gmail.com>
2019-09-27 23:40:46 -0500

    21th commit

--- android/app/src/main/gen/com/example/seemur_v1/R.java
+++ android/app/src/main/gen/com/example/seemur_v1/R.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package com.example.seemur_v1;
+package com.seemur.seemur_v1;
 
 /* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
 public final class R {
