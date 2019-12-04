f72a71dbd1d4b1a9f9167020b9564e3d5b637145
tostaco <jeusmag86@gmail.com>
2019-12-04 10:59:26 -0500

    44th commit

--- lib/src/providers/push_notifications_provider.dart
+++ lib/src/providers/push_notifications_provider.dart
@@ -15,23 +15,30 @@ class PushNotificationProvider {
       print(token);
       //dQcyWluzk6k:APA91bHEhPB9k0EEbCET-HQra3yRsvMzKx3mHMQxd2cUYE0Ci1DvWdivxAjoaHQm9RLKIHwQdLJmowv1NyLAJ-bodShG7VmRxeOtbifiGb-iJIur4yJK2JQ821Vt5Y3MGT3-NRG9Hw2r
     });
-    _firebaseMessaging.configure(onMessage: (info) {
+    _firebaseMessaging.configure(onMessage: (info) async {
       print('=====on Messagge======');
       print(info);
       String argumento = 'no-data';
       if (Platform.isAndroid) {
         argumento = info['data']['datos'] ?? 'no-data';
+      } else {
+        argumento = info['datos'] ?? 'no-data';
       }
       _mensajesStreamController.sink.add(argumento);
-    }, onLaunch: (info) {
+    }, onLaunch: (info) async {
       print('=====on Launch======');
       print(info);
-    }, onResume: (info) {
+    }, onResume: (info) async {
       print('=====on Resume======');
       print(info);
 
-      final noti = info['data']['datos'];
-      _mensajesStreamController.sink.add(noti);
+      String argumento = 'no-data';
+      if (Platform.isAndroid) {
+        argumento = info['data']['datos'] ?? 'no-data';
+      } else {
+        argumento = info['datos'] ?? 'no-data';
+      }
+      _mensajesStreamController.sink.add(argumento);
     });
   }
 
