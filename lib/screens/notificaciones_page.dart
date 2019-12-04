270cc8371d4116f7f59b97e8d813ca5c83603638
tostaco <jeusmag86@gmail.com>
2019-12-04 07:32:36 -0500

    43th commit

---
+++ lib/screens/notificaciones_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotificacionesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final argumentonotifi = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(107.0),
+        child: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          backgroundColor: Color.fromRGBO(22, 32, 44, 1),
+          title: Padding(
+            padding: const EdgeInsets.only(top: 30.0),
+            child: Text(
+              'Notificaciones',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          leading: Padding(
+            padding: const EdgeInsets.only(top: 22.0),
+            child: new IconButton(
+              icon: new Icon(
+                CupertinoIcons.back,
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+        ),
+      ),
+      body: ListTile(
+        leading: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset('assets/icon/icon.png')),
+        title: Text(argumentonotifi) ?? 'Sin notificaciones',
+      ),
+    );
+  }
+}
---
+++ lib/src/providers/push_notifications_provider.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationProvider {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final _mensajesStreamController = StreamController<String>.broadcast();
+  Stream<String> get mensajes => _mensajesStreamController.stream;
+  initNotificatios() {
+    _firebaseMessaging.requestNotificationPermissions();
+
+    _firebaseMessaging.getToken().then((token) {
+      print('=====FCM Token=====');
+      print(token);
+      //dQcyWluzk6k:APA91bHEhPB9k0EEbCET-HQra3yRsvMzKx3mHMQxd2cUYE0Ci1DvWdivxAjoaHQm9RLKIHwQdLJmowv1NyLAJ-bodShG7VmRxeOtbifiGb-iJIur4yJK2JQ821Vt5Y3MGT3-NRG9Hw2r
+    });
+    _firebaseMessaging.configure(onMessage: (info) {
+      print('=====on Messagge======');
+      print(info);
+      String argumento = 'no-data';
+      if (Platform.isAndroid) {
+        argumento = info['data']['datos'] ?? 'no-data';
+      }
+      _mensajesStreamController.sink.add(argumento);
+    }, onLaunch: (info) {
+      print('=====on Launch======');
+      print(info);
+    }, onResume: (info) {
+      print('=====on Resume======');
+      print(info);
+
+      final noti = info['data']['datos'];
+      _mensajesStreamController.sink.add(noti);
+    });
+  }
+
+  dispose() {
+    _mensajesStreamController?.close();
+  }
+}
