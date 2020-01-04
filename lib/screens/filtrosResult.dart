8d4abb24d48641eba48408950e492f8e61021255
UsmaJulian <jeusmag86@gmail.com>
2020-01-04 14:24:53 -0500

    52th commit

---
+++ lib/screens/filtrosResult.dart
@@ -0,0 +1,240 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:seemur_v1/auth/auth.dart';
+import 'package:seemur_v1/components/widgets/searchbar.dart';
+import 'package:seemur_v1/models/user_model.dart';
+import 'package:seemur_v1/utilidades/constantes.dart';
+
+class Filtrosresult extends StatefulWidget {
+  final BaseAuth auth;
+  final newList;
+
+  Filtrosresult({
+    this.auth,
+    this.newList,
+  });
+
+  @override
+  _FiltrosresultState createState() => _FiltrosresultState();
+}
+
+class _FiltrosresultState extends State<Filtrosresult>
+    with WidgetsBindingObserver {
+  final databaseReference = Firestore.instance;
+  String usuario = 'Usuario'; //user
+  String usuarioEmail = 'Email'; //userEmail
+  String id;
+  final formKey = GlobalKey<FormState>();
+  String _itemCiudad;
+  List<DropdownMenuItem<String>> _ciudadItems;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+    widget.auth.infoUser().then((onValue) {
+      setState(() {
+        usuario = onValue.displayName;
+        usuarioEmail = onValue.email;
+        id = onValue.uid;
+        print('ID $id');
+
+        _ciudadItems = getCiudadItems();
+        _itemCiudad = _ciudadItems[0].value;
+      });
+    });
+  }
+
+  getData() async {
+    return await Firestore.instance.collection('ciudades').getDocuments();
+  }
+
+  //Dropdownlist from firestore
+  List<DropdownMenuItem<String>> getCiudadItems() {
+    List<DropdownMenuItem<String>> items = List();
+    QuerySnapshot dataCiudades;
+    getData().then((data) {
+      dataCiudades = data;
+      dataCiudades.documents.forEach((obj) {
+        print('${obj.documentID} ${obj['nombre']}');
+        items.add(DropdownMenuItem(
+          value: obj.documentID,
+          child: Text(obj['nombre'],
+              style: TextStyle(
+                fontFamily: 'OpenSans',
+                color: Color(0xffffffff),
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal,
+                letterSpacing: 0,
+              )),
+        ));
+      });
+    }).catchError((error) => print('hay un error.....' + error));
+
+    items.add(DropdownMenuItem(
+      value: '0',
+      child: Text('Ciudad ',
+          style: TextStyle(
+            fontFamily: 'OpenSans',
+            color: Color(0xffffffff),
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            fontStyle: FontStyle.normal,
+            letterSpacing: 0,
+          )),
+    ));
+
+    return items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var lista = widget.newList;
+
+    for (var i = 0; i < lista.length; i++) {
+      lista[i] = "'${lista[i]}'";
+    }
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: new BoxDecoration(color: Color(0xff16202c)),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 0, top: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text(
+                          "¿Cuál es tu plan?",
+                          style: TextStyle(
+                            fontFamily: 'HankenGrotesk',
+                            color: Color(0xffffffff),
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.normal,
+                            letterSpacing: -0.4000000059604645,
+                          ),
+                        ),
+                        FutureBuilder(
+                          future: usersRef.document(id).get(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+
+                            Usuario usuario = Usuario.fromDoc(snapshot.data);
+
+                            return CircleAvatar(
+                                radius: 40.0,
+                                backgroundColor: Colors.grey,
+                                backgroundImage: usuario.foto.isEmpty
+                                    ? AssetImage(
+                                        'assets/images/Contenedordeimagenes.jpg')
+                                    : CachedNetworkImageProvider(usuario.foto));
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 0, left: 0.0, right: 201.0),
+                    child: SizedBox(
+                      width: 100,
+                      child: Theme(
+                        data: Theme.of(context).copyWith(
+                          canvasColor: Color(0xff16202c),
+                        ),
+                        child: DropdownButtonFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none, isDense: true),
+
+                          validator: (value) => value == '0'
+                              ? 'Debe seleccionar una ciudad'
+                              : null,
+
+                          value: _itemCiudad,
+
+                          items: _ciudadItems,
+
+                          onChanged: (value) {
+                            setState(() {
+                              _itemCiudad = value;
+                            });
+                          },
+
+                          //seleccionarCiudadItem,
+
+                          onSaved: (value) => _itemCiudad = value,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Column(
+                      children: <Widget>[
+                        SearchBar(),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                ],
+              ),
+            ),
+            StreamBuilder(
+                stream: Firestore.instance
+                    .collection('client')
+                    .where('taskplans', arrayContainsAny: lista)
+                    //             .where('taskenvironments', arrayContainsAny: (lista))
+                    //                .where('taskservices', isGreaterThanOrEqualTo:[(widget.newList.toString().replaceAll("[", "").replaceAll("]", ""))])
+//                  .where('tasktags',isGreaterThanOrEqualTo:[(widget.newList.toString().replaceAll("[", "").replaceAll("]", ""))])
+                    //               .where('taskfeatures',
+//                 isGreaterThanOrEqualTo:[(widget.newList.toString().replaceAll("[", "").replaceAll("]", ""))])
+//                  .where('tasktime',  isGreaterThanOrEqualTo:[(widget.newList.toString().replaceAll("[", "").replaceAll("]", ""))])
+//                  .where('taskprice', isGreaterThanOrEqualTo:[(widget.newList.toString().replaceAll("[", "").replaceAll("]", ""))])
+//                 .where('taskpayment', isGreaterThanOrEqualTo:[(widget.newList.toString().replaceAll("[", "").replaceAll("]", ""))])
+                    .snapshots(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data != null) {
+                      print('hola' + lista);
+                      return Flexible(
+                        child: ListView.builder(
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (BuildContext context, index) {
+                            return Column(
+                              children: <Widget>[
+                                Text(snapshot.data.documents[index]['taskname']
+                                    .toString()),
+                              ],
+                            );
+                          },
+                        ),
+                      );
+                    } else {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 28),
+                        child: Center(child: new CircularProgressIndicator()),
+                      );
+                    }
+                  }
+                  return Text('');
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/storageService.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:seemur_v1/utilidades/constantes.dart';
+import 'package:uuid/uuid.dart';
+
+class StorageService {
+  static Future<String> uploadUserProfileImage(
+      String url, File imageFile) async {
+    String photoId = Uuid().v4();
+    File image = await compressImage(photoId, imageFile);
+
+    if (url.isNotEmpty) {
+      print('url de la imagen-----');
+      print(url);
+// updating user profile image
+      RegExp exp = RegExp(r'userProfile_(.*).jpg');
+      photoId = exp.firstMatch(url)[1];
+      print(photoId);
+    }
+
+    StorageUploadTask uploadTask = storageRef
+        .child('images/users/userProfile_$photoId.jpg')
+        .putFile(image);
+    StorageTaskSnapshot storageSnap = await uploadTask.onComplete;
+    String downloadUrl = await storageSnap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  static Future<File> compressImage(String photoId, File image) async {
+    final tempDir = await getTemporaryDirectory();
+    final path = tempDir.path;
+    File compressedImageFile = await FlutterImageCompress.compressAndGetFile(
+      image.absolute.path,
+      '$path/img_$photoId.jpg',
+      quality: 70,
+    );
+    return compressedImageFile;
+  }
+}
